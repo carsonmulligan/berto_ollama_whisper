3a97ebf3364c54bcbb7f12b9fa260cab26133bbd
Carson Mulligan <103183546+carsonmulligan@users.noreply.github.com>
2024-09-22 05:24:58 -0400

    add run whisper sh

---
+++ run_whisper.sh
@@ -0,0 +1,15 @@
+# Install vLLM from pip:
+pip install vllm
+
+# Load and run the model:
+vllm serve "clu-ling/whisper-small-spanish"
+
+# Call the server using curl:
+curl -X POST "http://localhost:8000/v1/chat/completions" \ 
+	-H "Content-Type: application/json" \ 
+	--data '{
+		"model": "clu-ling/whisper-small-spanish"
+		"messages": [
+			{"role": "user", "content": "Hello!"}
+		]
+	}'
