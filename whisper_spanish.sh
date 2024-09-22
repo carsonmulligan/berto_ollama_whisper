ba0993ebada12a652dd240af98b602b033232e04
Carson Mulligan <103183546+carsonmulligan@users.noreply.github.com>
2024-09-22 05:37:30 -0400

    f

--- whisper_spanish.sh
+++ whisper_spanish.sh
@@ -3,7 +3,7 @@ conda create -n vllm python=3.11
 conda activate vllm
 
 # Install vLLM from pip:
-pip install vllm
+conda install vllm
 
 # Load and run the model:
 vllm serve "clu-ling/whisper-small-spanish"
@@ -14,6 +14,6 @@ curl -X POST "http://localhost:8000/v1/chat/completions" \
 	--data '{
 		"model": "clu-ling/whisper-small-spanish"
 		"messages": [
-			{"role": "user", "content": "Hello!"}
+			{"role": "user", "content": "¿Cómo va la vida, compa? Que tal tu día? tu vaya bien, mano?"}
 		]
 	}'
